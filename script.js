b2917a855c1c6afc5313c2ed60bad87b2244fcd9
chrisdanieloffical <boseolajide43@gmail.com>
2023-12-23 13:17:15 +0100

    Done the rps-ui

--- script.js
+++ script.js
@@ -1,76 +1,78 @@
-//script.js
-function getComputerChoice() {
- const randomNumber = Math.floor(Math.random() * 3)
- switch (randomNumber) {
- 	case 0:
- 		return 'rock';
- 	case 1:
- 		return 'paper';
- 	case 2:
- 		return 'scissors';
- 	default:
- 		console.error('Error: Invalid random Number');
- 		return null;
- }
-}
-
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
-	playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
 
-	//check for a tie
-	if (playerSelection === computerSelection.toLowerCase()) {
-		return "It a tie! Replay the round.";
-	}
+    // check for a tie
+    if (playerSelection === computerSelection.toLowerCase()) {
+        return "It's a tie! Replay the round.";
+    }
 
-	// determine the winner
-	if (
-		(playerSelection === 'rock' && computerSelection === 'scissors') ||
+    // determine the winner
+    if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')
-    ){
-		return `You Win! ${playerSelection} beats ${computerSelection}`;
-	} else {
-		return`You Lose! ${computerSelection} beats ${playerSelection}.`;
-	}
-
-}
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let round = 1; round <= 5; round++) {
-        const playerChoice = prompt('Enter your choice (Rock, Paper, or Scissors):');
-        const computerChoice = getComputerChoice();
-        
-        console.log(`Round ${round}:`);
-        console.log(`You chose: ${playerChoice}`);
-        console.log(`Computer chose: ${computerChoice}`);
-        
-        const result = playRound(playerChoice, computerChoice);
-        console.log(result);
-
-        // Update scores
-        if (result.includes('Win')) {
-            playerScore++;
-        } else if (result.includes('Lose')) {
-            computerScore++;
-        }
-
-        console.log(`Score - You: ${playerScore}, Computer: ${computerScore}\n`);
-    }
-
-    // Determine the winner of the game
-    if (playerScore > computerScore) {
-        console.log('Congratulations! You win the game!');
-    } else if (playerScore < computerScore) {
-        console.log('Sorry, you lose the game.');
+    ) {
+        playerScore++;
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else {
-        console.log("It's a tie! No clear winner.");
+        computerScore++;
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`;
     }
 }
 
-// Call the game function to start playing
-game();
+// Get references to the buttons and result/score displays
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
+const resultDisplay = document.getElementById("resultDisplay");
+const scoreDisplay = document.getElementById("scoreDisplay");
 
+// Add event listeners to the buttons
+rockBtn.addEventListener("click", function () {
+    const result = playRound("rock", computerSelection());
+    updateDisplay(result);
+});
+
+paperBtn.addEventListener("click", function () {
+    const result = playRound("paper", computerSelection());
+    updateDisplay(result);
+});
+
+scissorsBtn.addEventListener("click", function () {
+    const result = playRound("scissors", computerSelection());
+    updateDisplay(result);
+});
+
+// Function to generate computer selection (rock, paper, or scissors)
+function computerSelection() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+// Function to update the display with results and scores
+function updateDisplay(result) {
+    resultDisplay.textContent = result;
+    scoreDisplay.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+
+    // Check for a winner
+    if (playerScore === 5 || computerScore === 5) {
+        announceWinner();
+    }
+}
+
+// Function to announce the winner
+function announceWinner() {
+    if (playerScore === 5) {
+        resultDisplay.textContent = "Congratulations! You are the winner!";
+    } else {
+        resultDisplay.textContent = "Computer wins. Better luck next time!";
+    }
+
+    // Reset scores for a new game
+    playerScore = 0;
+    computerScore = 0;
+}
